7501d84c14fdb175f741fe5ab650b1b87e11c896
lvnguyen.ivs-1217 <lvnguyen.1217@indivisys.jp>
2022-08-23 13:53:08 +0700

    create react webapp

---
+++ src/component/loading.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class loadding extends Component {
+  render() {
+    return ( 
+        <div className='loadding'></div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Loading from '../src/component/loading'
+
+function App() {
+    return (
+        <div>
+            <h1>Hello anh em !</h1>
+            <Loading />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
